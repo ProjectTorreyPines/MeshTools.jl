be80cc4f2f1aa45cd95cf15e5bebecca19762446
Orso Meneghini <orso82@gmail.com>
2024-10-28 21:19:47 -0700

    Add analytics

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ end
 
 makedocs(;
     modules=[MeshTools],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(;analytics="G-65D8V8C8VQ"),
     sitename="MeshTools",
     checkdocs=:none,
     pages=["index.md", "api.md", "License" => "license.md", "Notice" => "notice.md"]
